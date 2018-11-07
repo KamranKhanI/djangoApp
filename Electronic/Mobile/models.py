b63923aba114c638697b829d7ce1cbacc7ebc5be
KamranKhanl <programe000@gmail.com>
2018-11-08 04:16:25 +0500

    Show mobile data in page
    make model and show its data in table

--- Electronic/Mobile/models.py
+++ Electronic/Mobile/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
+from datetime import date
 
-# Create your models here.
+
+
+class Mobile(models.Model):
+    company=models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    modelno = models.CharField(max_length=50)
+    condition=models.PositiveSmallIntegerField(max_length=25, validators=[MinValueValidator(10), MaxValueValidator(10)])
+    price=models.FloatField(max_length=20)
+    buy=models.DateField(default=date.today())
+
+    def __str__(self):
+        return self.company+'-'+self.modelno
+
--- Electronic/Mobile/views.py
+++ Electronic/Mobile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from Mobile.models import Mobile as mob
 
 # Create your views here.
 
@@ -7,3 +8,20 @@ def test(request):
     msg="HI IN NEW THEEM"
 
     return render(request,'Mobile/mobile.html',{'form':msg})
+
+
+def Mobile (request):
+
+
+
+    if (request.method=='POST'):
+
+        return render(request, 'Mobile/newmobile.html', {})
+
+    else:
+        mydata = mob.objects.all()
+
+
+
+
+        return render(request,'Mobile/newmobile.html',{'data':mydata})
